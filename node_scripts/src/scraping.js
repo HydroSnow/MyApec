3531690c2c0f5e9b1de54ff0cbb3af3f37f959d3
Alexis Trupin <link9745@gmail.com>
2019-12-16 19:15:49 +0100

    adding society, contract and city scraping

--- node_scripts/src/scraping.js
+++ node_scripts/src/scraping.js
@@ -3,6 +3,10 @@ cheerio = require('cheerio');
 this.parse = function(html, keywords) {
     let parsed = {};
     parsed.date = this.get_date(html);
+    let scc = this.get_scc(html);
+    parsed.contract = scc.contract;
+    parsed.society = scc.society;
+    parsed.city = scc.city;
     return parsed;
 }
 
@@ -10,4 +14,21 @@ this.get_date = function(html) {
     let text = cheerio('.date-offre', html)[0].children[0].data.split(" ")[2];
     let part = text.split("/");
     return new Date(part[2], part[1] - 1, part[0]);
+}
+
+this.get_scc = function(html) {
+    let cells = cheerio('.details-offer-list > li', html);
+    if (cells.length == 3) {
+        return {
+            society: cells[0].children[0].data,
+            contract: cells[1].children[4].children[0].data,
+            city: cells[2].children[0].data
+        };
+    } else {
+        return {
+            society: null,
+            contract: cells[0].children[4].children[0].data,
+            city: cells[1].children[0].data
+        };
+    }
 }
