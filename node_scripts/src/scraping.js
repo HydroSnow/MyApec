9e4188ebf713e83227664c699114190af3a1d661
Alexis Trupin <link9745@gmail.com>
2019-12-16 20:14:57 +0100

    adding skills scraping

--- node_scripts/src/scraping.js
+++ node_scripts/src/scraping.js
@@ -3,6 +3,7 @@ cheerio = require('cheerio');
 this.parse = function(html, keywords) {
     let parsed = {};
     parsed.date = this.get_date(html);
+    parsed.skills = this.get_skills(html, keywords);
     let scc = this.get_scc(html);
     parsed.contract = scc.contract;
     parsed.society = scc.society;
@@ -17,6 +18,37 @@ this.get_date = function(html) {
     let text = cheerio('.date-offre', html)[0].children[0].data.split(" ")[2];
     let part = text.split("/");
     return new Date(part[2], part[1] - 1, part[0]);
+}
+
+this.get_skills = function(html, keywords) {
+    get_text = function(cells) {
+        let text = "";
+        for (let a = 0; a < cells.length; a++) {
+            let cell = cells[a];
+            if (cell.children !== undefined) {
+                text += get_text(cell.children);
+            } else {
+                text += cell.data + "\n";
+            }
+        }
+        return text;
+    }
+
+    let cells = cheerio('.details-post > p', html);
+    let text = get_text(cells).toLowerCase();
+    console.log(text);
+
+    let skills = [];
+    for (let a = 0; a < keywords.length; a++) {
+        let word = keywords[a].word.toLowerCase();
+        let id_skill = keywords[a].id_skill;
+        if (skills.includes(id_skill) == false) {
+            if (text.includes(word) == true) {
+                skills.push(id_skill);
+            }
+        }
+    }
+    return skills;
 }
 
 this.get_scc = function(html) {
