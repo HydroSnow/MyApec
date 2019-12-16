2d013b2ae6788a266f8c3468d8ba16efb353e1fc
Alexis Trupin <link9745@gmail.com>
2019-12-16 14:59:04 +0100

    adding database tables sql files

---
+++ database.sql
@@ -0,0 +1,31 @@
+
+CREATE TABLE `offers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `salary` int(11) DEFAULT NULL,
+  `experience` int(11) NOT NULL,
+  `city` varchar(256) NOT NULL,
+  `date` date NOT NULL,
+  `contract` varchar(256) NOT NULL,
+  `society` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `skills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `keywords` (
+  `id_skill` int(11) NOT NULL,
+  `word` varchar(256) NOT NULL,
+  FOREIGN KEY (`id_skill`) REFERENCES `skills` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `matches` (
+  `id_offer` int(11) NOT NULL,
+  `id_skill` int(11) NOT NULL,
+  PRIMARY KEY (`id_offer`,`id_skill`),
+  FOREIGN KEY (`id_offer`) REFERENCES `offers` (`id`),
+  FOREIGN KEY (`id_skill`) REFERENCES `skills` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
