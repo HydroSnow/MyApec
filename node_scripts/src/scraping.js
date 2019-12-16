281e05224ccdc07786ea2ee358ddd1a906bf2f8e
Alexis Trupin <link9745@gmail.com>
2019-12-16 19:15:15 +0100

    adding date scraping

--- node_scripts/src/scraping.js
+++ node_scripts/src/scraping.js
@@ -2,5 +2,12 @@ cheerio = require('cheerio');
 
 this.parse = function(html, keywords) {
     let parsed = {};
+    parsed.date = this.get_date(html);
     return parsed;
+}
+
+this.get_date = function(html) {
+    let text = cheerio('.date-offre', html)[0].children[0].data.split(" ")[2];
+    let part = text.split("/");
+    return new Date(part[2], part[1] - 1, part[0]);
 }
