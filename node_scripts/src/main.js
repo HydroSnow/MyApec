74a2d4d74b3ad7adf5dd38b8e6e2ab029c1fdc39
Alexis Trupin <link9745@gmail.com>
2019-12-16 20:47:28 +0100

    adding finished log when no matches are inserted

--- node_scripts/src/main.js
+++ node_scripts/src/main.js
@@ -77,6 +77,9 @@ async function insert_new(id, info) {
                 if (err) { console.error(id, err); }
                 console.log("Finished " + id);
             });
+            
+        } else {
+            console.log("Finished " + id);
         }
     });
 }
