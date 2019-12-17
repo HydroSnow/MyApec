d28c2aa2fba4d33a8386bba03c60cca7194754fc
Alexis Trupin <link9745@gmail.com>
2019-12-17 19:23:25 +0100

    adding database verification of id before scraping

--- node_scripts/src/main.js
+++ node_scripts/src/main.js
@@ -18,10 +18,10 @@ async function setup() {
     settings = JSON.parse(json);
     database = mysql.createPool(settings.mysql);
     browser = await puppeteer.launch();
-    database.query("SELECT * FROM keywords", function(error, results, fields) {
-        if (error) {
-            throw error;
-        }
+    database.query({
+        sql: "SELECT * FROM keywords",
+    }, function(error, results, fields) {
+        if (error) { throw error; }
         keywords = results;
         start();
     });
@@ -33,9 +33,7 @@ async function start() {
     web.listen(async function(search_link) {
         try {
             let page = await browser.newPage();
-            await page.goto(search_link, {
-                timeout: 0
-            });
+            await page.goto(search_link, { timeout: 0 });
             let html = await page.content();
             page.close();
 
@@ -44,18 +42,25 @@ async function start() {
             let seen = {};
             for (let a = 0; a < links.length; a++) {
                 let link = links[a].attribs.href;
-                if (link == undefined) {
-                    continue;
-                }
+                if (link == undefined) { continue; }
                 let id = link.split(/[\/\?]/g)[5];
                 link = url.resolve(search_link, link);
                 if (seen[id] == undefined && id != null && id.length == 10) {
                     seen[id] = true;
-                    process_link(id, link);
+                    database.query({
+                        sql: "SELECT (`id`) FROM offers WHERE `id` = ?",
+                        values: [ id ]
+                    }, function(error, results, fields) {
+                        if (results.length == 0) {
+                            process_link(id, link);
+                        } else {
+                            console.log(id + " is already recorded");
+                        }
+                    });
                 }
             }
-        } catch (err) {
-            console.error(err);
+        } catch (error) {
+            console.error(error);
         }
     }, 42568);
 
@@ -66,17 +71,24 @@ async function start() {
             let seen = {};
             for (let a = 0; a < links.length; a++) {
                 let link = links[a].attribs.href;
-                if (link == undefined) {
-                    continue;
-                }
+                if (link == undefined) { continue; }
                 let id = new URL(link).searchParams.get("p2");
                 if (seen[id] == undefined && id != null && id.length == 10) {
                     seen[id] = true;
-                    process_link(id, link);
+                    database.query({
+                        sql: "SELECT (`id`) FROM offers WHERE `id` = ?",
+                        values: [ id ]
+                    }, function(error, results, fields) {
+                        if (results.length == 0) {
+                            process_link(id, link);
+                        } else {
+                            console.log(id + " is already recorded");
+                        }
+                    });
                 }
             }
-        } catch (err) {
-            console.error(err);
+        } catch (error) {
+            console.error(error);
         }
     }, 42569);
 }
@@ -85,41 +97,35 @@ async function process_link(id, link) {
     try {
         console.log("Processing " + id);
         let page = await browser.newPage();
-        await page.goto(link, {
-            timeout: 0
-        });
+        await page.goto(link, { timeout: 0 });
         let html = await page.content();
         page.close();
 
         let info = scraping.parse(html, keywords);
         insert_new(id, info);
 
-    } catch (err) {
-        console.error(id, err);
+    } catch (error) {
+        console.error(id, error);
     }
 }
 
 async function insert_new(id, info) {
-    database.query("INSERT INTO offers (`id`, `salary`, `experience`, `city`, `date`, `contract`, `society`) VALUES ?", [
-        [
-            [
-                id, info.salary, info.experience, info.city, info.date, info.contract, info.society
-            ]
-        ]
-    ], function(err, result) {
-        if (err) {
-            console.error(err);
-        }
+    database.query({
+        sql: "INSERT INTO offers (`id`, `salary`, `experience`, `city`, `date`, `contract`, `society`) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        values: [ id, info.salary, info.experience, info.city, info.date, info.contract, info.society ]
+    }, function(error, results, fields) {
+        if (error) { console.error(id, error); return; }
 
         if (info.skills.length !== 0) {
             let matches = [];
             info.skills.forEach(function(x) {
                 matches.push([id, x]);
             });
-            database.query("INSERT INTO matches (`id_offer`, `id_skill`) VALUES ?", [matches], function(err, result) {
-                if (err) {
-                    console.error(id, err);
-                }
+            database.query({
+                sql: "INSERT INTO matches (`id_offer`, `id_skill`) VALUES ?",
+                values: [ matches ]
+            }, function(error, results, fields) {
+                if (error) { console.error(id, error); return; }
                 console.log("Finished " + id);
             });
 
